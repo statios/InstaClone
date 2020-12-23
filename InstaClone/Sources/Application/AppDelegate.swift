f55552527419206d1048e1e5b5e2af77c0890e3a
Stat <statios@kakao.com>
2020-12-23 21:08:58 +0900

    Remove storyboard and etc

---
+++ InstaClone/Sources/Application/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  InstaClone
+//
+//  Created by Stat on 2020/12/23.
+//
+
+import UIKit
+import Then
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    
+    window = UIWindow().then {
+      $0.frame = UIScreen.main.bounds
+      $0.rootViewController = UIViewController()
+      $0.makeKeyAndVisible()
+    }
+    
+    return true
+  }
+  
+}
+
